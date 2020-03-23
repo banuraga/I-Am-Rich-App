bd8c0428bb4757c86cd06b1443b98c88b95f71aa
Banuraga <andika_w@hotmail.com>
2020-03-23 22:23:23 +0900

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Andika Banuraga on 26/08/18.
+//  Copyright © 2018 Andika Banuraga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
